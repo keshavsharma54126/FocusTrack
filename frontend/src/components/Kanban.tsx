ded5566ddc7c635068a4950096d896859f7589c4
unknown <sharmakeshav54126@gmail.com>
2024-08-24 21:08:44 +0530

    big fix

--- frontend/src/components/Kanban.tsx
+++ frontend/src/components/Kanban.tsx
@@ -24,7 +24,7 @@ const Kanban = () => {
           imageUrl: user?.imageUrl,
         }
       );
-
+      console.log();
       setUserId(res.data.id);
     } catch (e) {
       console.log("error while fetching user from clerk");
